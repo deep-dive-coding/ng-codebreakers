27aa3c5f745766253be856c46bb07a66260feb1d
georgekephart <gkephart@cnm.edu>
2021-04-18 23:28:54 -0600

    Added docblocks to both components

--- ng/src/app/codes/codes.component.ts
+++ ng/src/app/codes/codes.component.ts
@@ -14,10 +14,26 @@ import {Router} from '@angular/router';
 
 
 export class CodesComponent implements OnInit {
-	public codes: Code[] = [];
+	/**
+	 * State variable containing all of the codes returned by the server
+	 * @var Codes[] codes and array containing all the code objects that exists on the server
+	 */
+	public codes: Code[] = []
+	/**
+	 * Reactive form for submitting new code objects to the server
+	 */
 	public codeForm: FormGroup;
 
-	constructor(private codesService: CodeService, private formBuilder: FormBuilder, private router: Router) {
+
+	/**
+	 * constructor method for the codes component.The constructor is called when bootstrapping the component
+	 * and is called before ngOnInit. Values not related to the lifecycle of the component should be defined and set in
+	 * the constructor.
+	 * @param codeService HTTP service that interacts with the code endpoint on the server
+	 * @param formBuilder provided by angular for the creation of Reactive forms
+	 * @param router
+	 */
+	constructor(private codeService: CodeService, private formBuilder: FormBuilder, private router: Router) {
 		this.codeForm = this.formBuilder.group({
 			pool: ['', [Validators.required]],
 			length: ['', [Validators.required]]
@@ -25,26 +41,44 @@ export class CodesComponent implements OnInit {
 		})
 	}
 
+	/**
+	 * Initialization lifecycle method, Used to update values after asynchronous tasks have resolved. NgOnInit is made available
+	 * through the OnInit interface
+	 */
 	ngOnInit() {
-		this.loadAllCodes();
+		this.setAllCodes();
 	}
 
-	loadAllCodes(): void {
-		this.codesService.getAllCodes().subscribe(reply => {
+	/**
+	 * Simple mutator method that interacts with the code service to grab all the codes from the server and sets
+	 * them to the codes state variable
+	 */
+	setAllCodes(): void {
+		this.codeService.getAllCodes().subscribe(reply => {
 			this.codes = reply
 		})
 	}
 
+	/**
+	 * Method for handling the redirect to the detailed-code view for a specific code when the user clicks the detailed
+	 * information button for a specific code
+	 * when an end user clicks on the detailed information
+	 *
+	 * @param code Code The code object that will be displayed in the detailed code view
+	 */
 	navigateToDetailedView(code: Code): void {
 		this.router.navigate(['/detailed-code/', code.id]).catch(error => {
 			console.error(error)
 		})
-
-
 	}
 
+	/**
+	 * Method called by the code form on submission of the form in the codes component.
+	 * This method interacts with codeService createCode method to preform a post request to server
+	 */
 	createCode(): void {
 
+		// initial code object that is going to be sent to the server
 		const code: Code = {
 			id: null,
 			created: null,
@@ -54,9 +88,11 @@ export class CodesComponent implements OnInit {
 			solved: null,
 			text: null,
 		}
-		this.codesService.createCode(code).subscribe(response => {
+
+		//method call that actually preforms the http request.
+		this.codeService.createCode(code).subscribe(response => {
 			this.codeForm.reset()
-			this.loadAllCodes()
+			this.setAllCodes()
 			this.navigateToDetailedView(response)
 		})
 	}
--- ng/src/app/detailed-code/detailed-code.component.ts
+++ ng/src/app/detailed-code/detailed-code.component.ts
@@ -12,27 +12,73 @@ import {GuessService} from '../shared/services/guess.service';
 	// url to the template containing the html (view) for this component.
 	templateUrl: './detailed-code.component.html'
 })
-
+/**
+ * Main component for the detailed code view.
+ *
+ * This component uses the url parameter codeId to grab all the pertinent information for a specific code, the code and
+ * all the guesses related to code.
+ */
 export class DetailedCodeComponent implements OnInit {
+	/**
+	 * The code being displayed. The value is set on component initiation.
+	 */
 	public code: Code | null = null;
+	/**
+	 * Reactive Form for submitting a guess request for the code to the server,
+	 */
 	public guessForm: FormGroup;
-	public allGuesses: Guess[] = [];
+	/**
+	 * An array containing all the guesses returned from the server for the specific code
+	 */
+	public guesses: Guess[] = [];
+	/**
+	 * The response from the previous guess request submitted to the server.
+	 */
 	public previousGuess: Guess | null = null;
+	/**
+	 * The codeId grabbed from the url, the value is set in the constructor and cannot be updated after it has been initialized
+	 * @var  string codeId
+	 */
 	private readonly codeId: string;
 
-	constructor(private codesService: CodeService, private formBuilder: FormBuilder, private route: ActivatedRoute, private guessService: GuessService) {
+	/**
+	 * Constructor method for the detailed code component. The constructor is called when bootstrapping the component
+	 * and is called before ngOnInit. Values not related to the lifecycle of the component should be defined and set in
+	 * the constructor.
+	 *
+	 * @param codeService HTTP service that interacts with the code endpoint on the server
+	 * @param guessService Http service that interacts with the guess endpoint on the server
+	 * @param formBuilder provided by angular for the creation of Reactive forms
+	 * @param route Activated Route object provided by Angular to interact with the current route
+	 */
+	constructor(
+		private codeService: CodeService,
+		private guessService: GuessService,
+		private formBuilder: FormBuilder,
+		private route: ActivatedRoute
+	) {
+		// this.route.snapshot grabs pertinent url parameters of off the url on object instantiation.
 		this.codeId = <string>this.route.snapshot.paramMap.get('codeId')
+
+		//initializes the form for creating guesses. Form validation for the guess form is reinitialized on ngOnInit
 		this.guessForm = this.formBuilder.group({
 			guess: ['', [Validators.required]],
-
 		})
 	}
 
+	/**
+	 * Initialization lifecycle method, Used to update values after they have been resolved. NgOnInit is made available
+	 * through the OnInit interface
+	 */
 	ngOnInit() {
 		this.setCode(this.codeId)
-		this.setAllGuesses(this.codeId)
+		this.setGuessesByCodeId(this.codeId)
 	}
 
+	/**
+	 * simple method that allows for the guess form to be updated dynamically depending on the length of the code returned from the server.
+	 * @param length length  of the characters in the code. Used to update form validation for min and max length
+	 */
 	updateFormValidation(length: number): void {
 		const minLengthValidator = Validators.minLength(length);
 		const maxLengthValidator = Validators.maxLength(length);
@@ -41,16 +87,31 @@ export class DetailedCodeComponent implements OnInit {
 
 	}
 
+	/**
+	 * Simple mutator method that interacts with the code service to grab a specific code by code id and assign the value
+	 * to the code state variable as well as update thw guess form validation.
+	 * dynamically
+	 *
+	 * @param codeId The id of the code that is being set in the mutator
+	 */
 	setCode(codeId: string): void {
-		this.codesService.getCodeByCodeId(codeId).subscribe(response => {
+		this.codeService.getCodeByCodeId(codeId).subscribe(response => {
 			this.code = response;
+			//Since this is where the code is being returned from the server it seemed like the natural place to update
+			// validation for the form.
 			this.updateFormValidation(response.length);
 
 		})
-
 	}
 
+	/**
+	 * Method called by the guess form on submission of the form in the detailed code component.
+	 * This method interacts with the guessService's createGuest method to preform a post request to the server.
+	 *
+	 * @param codeId the codeId for the code the guess based on.
+	 */
 	createGuess(codeId: string | null): void {
+		// initial guess object that is going to be sent to the server.
 		const guess: Guess = {
 			id: null,
 			created: null,
@@ -62,16 +123,21 @@ export class DetailedCodeComponent implements OnInit {
 
 		this.guessService.createGuess(<string>codeId, guess).subscribe(response => {
 			this.previousGuess = response
-			this.setAllGuesses(this.codeId)
+			this.setGuessesByCodeId(this.codeId)
 			if (response.solution === true) {
 				this.setCode(this.codeId)
 			}
 		})
 	}
 
-	setAllGuesses(codeId: string | null): void {
+	/**
+	 * Simple mutator method that interacts with the guess service to grab all the guesses for a code from the server and sets
+	 * them to the guesses state variable
+	 * @param codeId the primary key of the code with which all the guesses are getting grabbed
+	 */
+	setGuessesByCodeId(codeId: string | null): void {
 		this.guessService.getGuessByCodeId(<string>codeId).subscribe(response => {
-			this.allGuesses = response
+			this.guesses = response
 		})
 	}
 
